de88ddd779021c960fe218962b8273875ce23b18
zaynabzaara <zainebzaara01@gmail.com>
2020-12-09 01:12:30 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css'
+
+class App extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      person: {
+        fullName: 'ZAARA Zaynab',
+        bio: "« La sagesse, c'est d'avoir des rêves suffisamment grands pour ne pas les perdre de vue lorsqu'on les poursuit. » - Oscar Wilde",
+        imgSrc: './fed.jpg',
+        profession: 'Full Stack Developer Engineer',
+      },
+
+      show: false,
+      counter: 0
+    }
+  }
+
+
+  change = () => {
+    this.setState({
+      show: !this.state.show
+    })
+  }
+  increment = () => {
+    const {counter}= this.state;
+    this.setState({counter: counter + 1})
+    
+  }
+
+  componentDidMount(){
+
+setInterval(this.increment, 1000) 
+  
 }
 
+  render() {
+
+
+    return (
+      <div className='App'>
+        Component Life Cycle : {this.state.counter} <br />
+        <button  onClick={this.change}> show profile</button>
+
+        {this.state.show ?
+          <div><br />
+            <p>{this.state.person.fullName}</p><br />
+            <p>{this.state.person.bio}</p><br />
+            <p>{this.state.person.profession}</p><br />
+            <img alt='' src={this.state.person.imgSrc} /><br />
+          </div> : <p>Veuillez cliquer le bouton</p>}
+
+
+      </div>
+
+    )
+  }
+}
 export default App;
